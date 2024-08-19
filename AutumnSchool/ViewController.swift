1fbaf32e6ac2ddfa9eab752cd3fe406ca7fc62ae
nelipau17 <ponchik0108@icloud.com>
2024-08-19 12:10:04 +0500

    Initial Commit

---
+++ AutumnSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutumnSchool
+//
+//  Created by Polina Popova on 19/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
